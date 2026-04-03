b4c9c1c41b9d2ac71724bbeb4fb790911f6cfe32
John King <jk@almostmachines.dev>
2026-04-03 17:35:44 +0900

    Export types

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,8 @@ mod local_event_bus;
 mod shared_event_bus;
 
 pub use local_event_bus::LocalEventBus;
+pub use local_event_bus::LocalEventStream;
+pub use local_event_bus::LocalEventValidator;
 pub use shared_event_bus::SharedEventBus;
+pub use shared_event_bus::SharedEventStream;
+pub use shared_event_bus::SharedEventValidator;
